aabb2b4ffa8d8a24656821d50b7a29a889bf3256
Mohan Bhimavarapu <bhimavarapumohansai@gmail.com>
2025-10-18 20:27:29 +0530

    ec2 code update null rsrc

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,28 +3,38 @@ resource "aws_security_group" "allow_ssh" {
   description = "Allow all SSH connections"
 
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    name = "Allow_SSH"
+    name       = "Allow_SSH"
     created_by = "Mohan Bhimavarapu"
   }
 
 }
- resource "aws_instance" "Test" {
+resource "aws_instance" "Test" {
 
-    ami = var.ami_id
-    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-    instance_type = var.instance_type
-    tags = var.tags
- }  
+  ami                    = var.ami_id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  instance_type          = var.instance_type
+  tags                   = var.tags
+}
+
+#So this null resource will help in not creating ec2 instance instaed it will exec prit msg
+resource "null_resource" "null_resource_example" {
+  triggers = {
+    id = aws_instance.Test.id
+  }
+  provisioner "local-exec" {
+    command = "echo Hello World"
+  }
+}
--- ec2/practice.tf
+++ ec2/practice.tf
@@ -1,25 +1,25 @@
 resource "aws_instance" "Test_VM" {
-  instance_type = "t3.micro"
-  count = 2
-  ami = "ami-00354acb6e3508fd0"
+  instance_type   = "t3.micro"
+  count           = 2
+  ami             = "ami-00354acb6e3508fd0"
   security_groups = [aws_security_group.my_sg.id]
 }
 resource "aws_security_group" "my_sg" {
   name = "my-security-group"
   ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "SSH"
-        cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "SSH"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-        from_port = 0
-        to_port = 0
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    name = "allow-ssh-connection"
+    name       = "allow-ssh-connection"
     created_by = "Mohan Anna"
   }
 }
